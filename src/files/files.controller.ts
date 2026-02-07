62f5c3fe80f1b7232c9391bf4d14f02ba1421b8a
Alexis <alexisreillo12@gmail.com>
2026-02-07 15:24:40 +0100

    Creamos funcionalidad para poder subir los archivos de forma estática al directorio (esto no se suele usar en entornos productivos)

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,15 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FilesService } from './files.service';
+import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
+import { diskStorage } from 'multer';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { type Response } from 'express';
 
+import { fileFilter, fileNamer } from './helpers';
+import { FilesService } from './files.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) { }
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) { }
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter: fileFilter,
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    })
+  }))
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-    return file;
+
+    if (!file) {
+      throw new BadRequestException('Make sure that the files is a image');
+    }
+
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`
+
+    return { secureUrl }
+  }
+
+
+  @Get('product/:name')
+  getFileByName(
+    @Res() res: Response,
+    @Param('name') name: string
+  ) {
+
+
+    const path = this.filesService.getImagePath(name);
+
+    res.sendFile(path);
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,6 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
 
+
+    getImagePath(name: string) {
+
+
+        const path = join(__dirname, '../../static/products', name);
+
+        if (!existsSync(path)) {
+            throw new BadRequestException('Not image found')
+        }
+
+        return path;
+
+    }
 }
